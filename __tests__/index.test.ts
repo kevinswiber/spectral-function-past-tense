96ee4875478eddf9fde840341e04b2d1d411d3fe
Kevin Swiber <kswiber@gmail.com>
2021-12-08 16:28:13 -0800

    Fixing tests, using the pre-defined function from Spectral kinda breaks things.

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -6,18 +6,6 @@ import ruleset from "./__fixtures__/.spectral";
 
 const fixturesPath = path.join(__dirname, "__fixtures__");
 
-it("should return void when an event is past tense", () => {
-  const result = past("LightShined");
-  expect(result).toBeFalsy();
-});
-
-it("should return a validation message when an event is not past tense", () => {
-  const result = past("LightShining");
-  expect(result).toBeDefined();
-  expect(result).toHaveLength(1);
-  expect(result).toContainEqual({ message: "Value must be past tense." });
-});
-
 it("should return a validation error on an AsyncAPI document", async () => {
   const subject = await readFile(path.join(fixturesPath, "streetlights-invalid.yaml"), "utf-8");
   const spectral = new Spectral();
@@ -25,7 +13,7 @@ it("should return a validation error on an AsyncAPI document", async () => {
   const results = await spectral.run(subject);
   expect(results).toContainEqual({
     code: "rule",
-    message: "Value must be past tense.",
+    message: `Value "LightMeasuring" must be past tense.`,
     path: ["channels", "light/measured", "publish", "message", "name"],
     range: { end: { character: 28, line: 20 }, start: { character: 14, line: 20 } },
     severity: 1,
